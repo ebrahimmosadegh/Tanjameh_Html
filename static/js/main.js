12c432d202940bdfdb5908d8699e919e93a03aa2
ebrahimmosadegh <ebrahimmosadegh@gmail.com>
2023-01-16 20:53:11 +0330

    add footer box

--- static/js/main.js
+++ static/js/main.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
       mouseDrag:false,
       touchDrag:false,
       autoplay:true,
-      autoplayTimeout:3000,
+      autoplayTimeout:4000,
       autoplayHoverPause:true,
       margin: 10,
       navText : ['<i class="fa-solid fa-arrow-right-long"></i>','<i class="fa-solid fa-arrow-left-long"></i>'],
@@ -21,7 +21,7 @@ jQuery(document).ready(function($) {
 var btn_play=$('.btn-play')
 var btn_stop=$('.btn-stop')
     $('.play').on('click',function(){
-owl_animIndex.trigger('play.owl.autoplay',[3000]);
+owl_animIndex.trigger('play.owl.autoplay',[4000]);
 btn_play.hide();
 btn_stop.fadeIn(500);
 })
