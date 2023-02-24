0db8a842163b7d30e10af4c92acf736eeb68e545
ebrahimmosadegh <ebrahimmosadegh@gmail.com>
2023-02-25 00:02:03 +0330

    create cart page

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,18 +1,26 @@
+// hide dropdown cart icon
+$(".cart").mouseenter(function () {
+    $("#cart-items").removeClass("d-block");
+    $("#bag-empty").removeClass("d-block");
+});
+
 /* Set rates + misc */
-var taxRate = 0.05;
+var taxRate = 0.09;
 var shippingRate = 15.00; 
 var fadeTime = 300;
 
-
 /* Assign actions */
-$('.product-quantity input').change( function() {
+$('.product-quantity select').change( function() {
   updateQuantity(this);
 });
 
-$('.product-removal button').click( function() {
+$('.product-removal a').click( function() {
   removeItem(this);
 });
 
+$('.wishList').click( function() {
+  removeItem(this);
+});
 
 /* Recalculate cart */
 function recalculateCart()
@@ -21,13 +29,25 @@ function recalculateCart()
   
   /* Sum up row totals */
   $('.product').each(function () {
-    subtotal += parseFloat($(this).children('.product-line-price').text());
+    subtotal += parseFloat($(this).find('.product-line-price').text());
   });
   
   /* Calculate totals */
   var tax = subtotal * taxRate;
   var shipping = (subtotal > 0 ? shippingRate : 0);
   var total = subtotal + tax + shipping;
+
+//If there is a valid promoCode, and subtotal < 10 subtract from total
+var promoPrice = parseFloat($('.promo-value').text());
+if (promoPrice) {
+  if (subtotal >= 10) {
+    total -= promoPrice;
+  } else {
+    alert('سفارش بایستی بیشتر از 10 تومان باشد');
+    $('.summary-promo').addClass('d-none');
+    $('.summary-promo').removeClass('d-flex');
+  }
+}
   
   /* Update totals display */
   $('.totals-value').fadeOut(fadeTime, function() {
@@ -37,6 +57,8 @@ function recalculateCart()
     $('#cart-total').html(total.toFixed(2));
     if(total == 0){
       $('.checkout').fadeOut(fadeTime);
+      $('.basket-exists').fadeOut(fadeTime);
+      $('.basket-null').fadeIn(fadeTime);
     }else{
       $('.checkout').fadeIn(fadeTime);
     }
@@ -44,18 +66,17 @@ function recalculateCart()
   });
 }
 
-
 /* Update quantity */
 function updateQuantity(quantityInput)
 {
   /* Calculate line price */
-  var productRow = $(quantityInput).parent().parent();
-  var price = parseFloat(productRow.children('.product-price').text());
+  var productRow = $(quantityInput).parent().parent().parent();
+  var price = parseFloat(productRow.find('.product-price').text());
   var quantity = $(quantityInput).val();
   var linePrice = price * quantity;
   
   /* Update line price display and recalc cart totals */
-  productRow.children('.product-line-price').each(function () {
+  productRow.find('.product-line-price').each(function () {
     $(this).fadeOut(fadeTime, function() {
       $(this).text(linePrice.toFixed(2));
       recalculateCart();
@@ -64,14 +85,49 @@ function updateQuantity(quantityInput)
   });  
 }
 
-
 /* Remove item from cart */
 function removeItem(removeButton)
 {
   /* Remove row from DOM and recalc cart total */
-  var productRow = $(removeButton).parent().parent();
+  var productRow = $(removeButton).parent().parent().parent().parent().parent();
   productRow.slideUp(fadeTime, function() {
     productRow.remove();
     recalculateCart();
+    //items basket
+    $(".items-basket").text(($("#list-item-product li").length));
   });
 }
+
+// promotion code
+var promoCode;
+var promoPrice;
+
+$('.promo-code-cta').click(function() {
+
+  promoCode = $('#promo-code').val();
+
+  if (promoCode == '10off' || promoCode == '10OFF') {
+    //If promoPrice has no value, set it as 10 for the 10OFF promocode
+    if (!promoPrice) {
+      promoPrice = 10;
+    } else if (promoCode) {
+      promoPrice = promoPrice * 1;
+    }
+  } else if (promoCode != '') {
+    $('.result-promo-code').text('کد تخفیف اشتباه میباشد، لطفا تصحیح نمایید');
+    promoPrice = 0;
+  }
+  //If there is a promoPrice that has been set (it means there is a valid promoCode input) show promo
+  if (promoPrice) {
+    $('.result-promo-code').text("کد تخفیف اعمال شد");
+    $('.summary-promo').removeClass('d-none');
+    $('.summary-promo').addClass('d-flex');
+    $('.promo-value').text(promoPrice.toFixed(2));
+    recalculateCart(true);
+  }
+});
+
+// input promotion show button cta
+$("#promo-code").on("input", function() {
+  $(this).next().toggle(this.value != "");
+});
